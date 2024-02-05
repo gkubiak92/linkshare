449804bc6ca213146a9350765a19b904fa640351
Grzegorz <gkubiak92@gmail.com>
2024-02-05 19:01:16 +0100

    improve pagination component

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,12 +2,14 @@
 import {
   Pagination as PaginationBase,
   PaginationContent,
+  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { usePathname, useSearchParams } from "next/navigation";
+import { ReactNode } from "react";
 
 type PaginationProps = {
   currentPage: number;
@@ -32,40 +34,107 @@ export const Pagination = ({ currentPage, pagesCount }: PaginationProps) => {
   const params = new URLSearchParams(searchParams);
   params.delete("page");
 
-  const pagesToShow = Array.from({ length: PAGES_TO_SHOW }, (_, index) => {
-    if (currentPage + PAGES_TO_SHOW > pagesCount) {
-      return pagesCount - PAGES_TO_SHOW + index + 1;
-    }
+  const paginationItems: ReactNode[] = [];
 
-    return currentPage + index;
-  });
+  let startPage = Math.max(1, currentPage - Math.floor(PAGES_TO_SHOW / 2));
+  let endPage = Math.min(pagesCount, startPage + PAGES_TO_SHOW - 1);
+
+  if (endPage - startPage < PAGES_TO_SHOW - 1) {
+    startPage = Math.max(1, endPage - PAGES_TO_SHOW + 1);
+  }
+
+  if (currentPage > 1) {
+    paginationItems.push(
+      <PaginationItem key="pagination-previous">
+        <PaginationPrevious href={getPageUrl(currentPage - 1, path, params)} />
+      </PaginationItem>,
+    );
+  }
+
+  if (currentPage > Math.floor(PAGES_TO_SHOW / 2) + 1) {
+    paginationItems.push(
+      <PaginationItem key="pagination-first-page">
+        <PaginationLink href={getPageUrl(1, path, params)}>{1}</PaginationLink>
+      </PaginationItem>,
+    );
+  }
+
+  if (startPage > 1 + Math.floor(PAGES_TO_SHOW / 2)) {
+    const leftEllipsisReplacement = 2;
+    if (startPage - 1 === leftEllipsisReplacement) {
+      paginationItems.push(
+        <PaginationItem key="pagination-left-ellipsis">
+          <PaginationLink
+            href={getPageUrl(leftEllipsisReplacement, path, params)}
+          >
+            {leftEllipsisReplacement}
+          </PaginationLink>
+        </PaginationItem>,
+      );
+    } else {
+      paginationItems.push(
+        <PaginationItem key="pagination-left-ellipsis">
+          <PaginationEllipsis />
+        </PaginationItem>,
+      );
+    }
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    paginationItems.push(
+      <PaginationItem key={`pagination-page-${i}`}>
+        <PaginationLink
+          href={getPageUrl(i, path, params)}
+          isActive={currentPage === i}
+        >
+          {i}
+        </PaginationLink>
+      </PaginationItem>,
+    );
+  }
+
+  if (endPage < pagesCount - Math.floor(PAGES_TO_SHOW / 2)) {
+    const rightEllipsisReplacement = pagesCount - 1;
+    if (endPage === rightEllipsisReplacement - 1) {
+      paginationItems.push(
+        <PaginationItem key="pagination-right-ellipsis">
+          <PaginationLink
+            href={getPageUrl(rightEllipsisReplacement, path, params)}
+          >
+            {rightEllipsisReplacement}
+          </PaginationLink>
+        </PaginationItem>,
+      );
+    } else {
+      paginationItems.push(
+        <PaginationItem key="pagination-right-ellipsis">
+          <PaginationEllipsis />
+        </PaginationItem>,
+      );
+    }
+  }
+
+  if (endPage < pagesCount - Math.floor(PAGES_TO_SHOW / 2) + 1) {
+    paginationItems.push(
+      <PaginationItem key="pagination-last-page">
+        <PaginationLink href={getPageUrl(pagesCount, path, params)}>
+          {pagesCount}
+        </PaginationLink>
+      </PaginationItem>,
+    );
+  }
+
+  if (currentPage < pagesCount) {
+    paginationItems.push(
+      <PaginationItem key="pagination-next">
+        <PaginationNext href={getPageUrl(currentPage + 1, path, params)} />
+      </PaginationItem>,
+    );
+  }
 
   return (
     <PaginationBase>
-      <PaginationContent>
-        {currentPage > 1 && (
-          <PaginationItem>
-            <PaginationPrevious
-              href={getPageUrl(currentPage - 1, path, params)}
-            />
-          </PaginationItem>
-        )}
-        {pagesToShow.map((page) => (
-          <PaginationItem key={page}>
-            <PaginationLink
-              href={getPageUrl(page, path, params)}
-              isActive={page === currentPage}
-            >
-              {page}
-            </PaginationLink>
-          </PaginationItem>
-        ))}
-        {currentPage < pagesCount && (
-          <PaginationItem>
-            <PaginationNext href={getPageUrl(currentPage + 1, path, params)} />
-          </PaginationItem>
-        )}
-      </PaginationContent>
+      <PaginationContent>{paginationItems}</PaginationContent>
     </PaginationBase>
   );
 };

eaa0ce186b21a9a77eb295f4a79b2aa615e5ac82
Grzegorz <gkubiak92@gmail.com>
2024-02-17 18:37:06 +0100

    add displaying user below each link entry

--- src/components/linkEntry/LinkEntry.tsx
+++ src/components/linkEntry/LinkEntry.tsx
@@ -51,27 +51,36 @@ export const LinkEntry = ({
       )}
       <span className="text-md md:text-xl capitalize">{title}</span>
     </CardHeader>
-    <CardContent className="flex items-start gap-4">
-      <div className="flex flex-col flex-1 max-w-full">
-        <CardDescription className="h-16 max-h-16 overflow-hidden line-clamp-3">
-          {description}
-        </CardDescription>
-        <Link
-          href={url}
-          className="block text-blue-600 text-xs mb-2 break-words"
-        >
-          {url}
-        </Link>
-        <div className="flex gap-2 flex-wrap mb-2">
-          {tags.map(({ name }, index) => (
-            <Link
-              key={index}
-              href={`${routes.links.tags}/${encodeURIComponent(name)}`}
-            >
-              <Chip className="block text-xs">#{name}</Chip>
-            </Link>
-          ))}
+    <CardContent className="flex flex-col">
+      <div className="flex items-start gap-4">
+        <div className="flex flex-col flex-1 max-w-full">
+          <CardDescription className="h-16 max-h-16 overflow-hidden line-clamp-3">
+            {description}
+          </CardDescription>
+          <Link
+            href={url}
+            className="block text-blue-600 text-xs mb-2 break-words"
+          >
+            {url}
+          </Link>
+          <div className="flex gap-2 flex-wrap mb-2">
+            {tags.map(({ name }, index) => (
+              <Link
+                key={index}
+                href={`${routes.links.tags}/${encodeURIComponent(name)}`}
+              >
+                <Chip className="block text-xs">#{name}</Chip>
+              </Link>
+            ))}
+          </div>
         </div>
+        {thumbnailUrl && (
+          <div className="hidden md:block relative w-44 h-28">
+            <LinkEntryThumbnail imageUrl={thumbnailUrl} />
+          </div>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
         <div className="flex items-center gap-2 text-2xl text-muted-foreground">
           <span className="block">🎯</span>
           <span className="block text-lg text-zinc-500">{score}</span>
@@ -82,12 +91,17 @@ export const LinkEntry = ({
             </>
           )}
         </div>
-      </div>
-      {thumbnailUrl && (
-        <div className="hidden md:block relative w-44 h-28">
-          <LinkEntryThumbnail imageUrl={thumbnailUrl} />
+        <div className="flex items-center gap-2">
+          <span className="block text-zinc-500 text-sm">{user.name}</span>
+          <Image
+            className="rounded-full"
+            src={user.image}
+            width={24}
+            height={24}
+            alt={`${user.name} avatar image`}
+          />
         </div>
-      )}
+      </div>
     </CardContent>
   </Card>
 );
